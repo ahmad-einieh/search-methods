a54f61603cfbec2e4ed820517d2f9570cc514b11
ahmad einieh <abomoaz3375@gmail.com>
2022-02-19 18:26:18 +0300

    Initial commit

---
+++ Search.py
@@ -0,0 +1,149 @@
+from queue import PriorityQueue
+
+dict_graph = {}
+
+# Read the data.txt file
+with open('cities.txt', 'r') as f:
+    for l in f:
+        city_a, city_b, p_cost = l.split()
+        if city_a not in dict_graph:
+            dict_graph[city_a] = {}
+        dict_graph[city_a][city_b] = float(p_cost)
+        if city_b not in dict_graph:
+            dict_graph[city_b] = {}
+        dict_graph[city_b][city_a] = float(p_cost)
+
+
+# Breadth First Search Method
+def BreadthFirstSearch(graph, src, dst):
+    count = 0
+    q = [(src, [src], 0)]
+    visited = {src}
+    while q:
+        #count = count + 1
+        (node, path, cost) = q.pop(0)
+        for temp in list(graph[node].keys()):
+            count = count + 1
+            if temp == dst:
+                return path + [temp], cost + graph[node][temp] , str(count)
+            else:
+                if temp not in visited:
+                    visited.add(temp)
+                    q.append((temp, path + [temp], cost + graph[node][temp]))
+
+
+
+# Depth First Search Method
+def DepthFirstSearch(graph, src, dst):
+    stack = [(src, [src], 0)]
+    visited = {src}
+    while stack:
+        (node, path, cost) = stack.pop()
+        for temp in list(graph[node].keys()):
+            if temp == dst:
+                return path + [temp], cost + graph[node][temp]
+            else:
+                if temp not in visited:
+                    visited.add(temp)
+                    stack.append((temp, path + [temp], cost + graph[node][temp]))
+
+
+# Iterative Deepening Search Method
+def IterativeDeepening(graph, src, dst):
+    level = 0
+    count = 0
+    stack = [(src, [src], 0)]
+    visited = {src}
+    while True:
+        level += 1
+        while stack:
+            if count <= level:
+                count = 0
+                (node, path, cost) = stack.pop()
+                for temp in list(graph[node].keys()):
+                    if temp == dst:
+                        return path + [temp], cost + graph[node][temp]
+                    else:
+                        if temp not in visited:
+                            visited.add(temp)
+                            count += 1
+                            stack.append((temp, path + [temp], cost + graph[node][temp]))
+            else:
+                q = stack
+                visited_bfs = {src}
+                while q:
+                    (node, path, cost) = q.pop(0)
+                    for temp in list(graph[node].keys()):
+                        if temp == dst:
+                            return path + [temp], cost + graph[node][temp]
+                        else:
+                            if temp not in visited_bfs:
+                                visited_bfs.add(temp)
+                                q.append((temp, path + [temp], cost + graph[node][temp]))
+                break
+
+def fun(item):
+    return item[2]
+def ucs2(graph, src, dst):
+    count = 0
+    q = [(src, [src], 0)]
+    visited = {src}
+    while q:
+        # count = count + 1
+        (node, path, cost) = q.pop(0)
+        for temp in list(graph[node].keys()):
+            count = count + 1
+            if temp == dst:
+                return path + [temp], cost + graph[node][temp], str(count)
+            else:
+                if temp not in visited:
+                    visited.add(temp)
+                    q.append((temp, path + [temp], cost + graph[node][temp]))
+                    q.sort(key=fun)
+
+
+n = 1
+print(dict_graph)
+print("------------------------------------------------")
+while n == 1:
+    x = eval(input("enter the type of search you want to do \n1.BFS 2.DFS 3.ID 4.UCS:: \n "))
+    if x == 1:
+        src = input("Enter the source: ")
+        dst = input("Enter the Destination: ")
+        while src not in dict_graph or dst not in dict_graph:
+            print("No such city name")
+            src = input("Enter the correct source (case_sensitive):\n")
+            dst = input("Enter the correct destination(case_sensitive):\n ")
+        print("for BFS")
+        print((BreadthFirstSearch(dict_graph, src, dst)))
+
+    elif x == 2:
+        src = input("Enter the source: ")
+        dst = input("Enter the Destination: ")
+        while src not in dict_graph or dst not in dict_graph:
+            print("No such city name")
+            src = input("Enter the correct source (case_sensitive):\n")
+            dst = input("Enter the correct destination(case_sensitive):\n ")
+        print("for DFS")
+        print((DepthFirstSearch(dict_graph, src, dst)))
+
+    elif x == 3:
+        src = input("Enter the source:")
+        dst = input("Enter the Destination: ")
+        while src not in dict_graph or dst not in dict_graph:
+            print("No such city name")
+            src = input("Enter the correct source (case_sensitive):\n")
+            dst = input("Enter the correct destination(case_sensitive):\n")
+        print("for ID")
+        print((IterativeDeepening(dict_graph, src, dst)))
+    elif x == 4:
+        src = input("Enter the source:")
+        dst = input("Enter the Destination: ")
+        while src not in dict_graph or dst not in dict_graph:
+            print("No such city name")
+            src = input("Enter the correct source (case_sensitive):\n")
+            dst = input("Enter the correct destination(case_sensitive):\n")
+        print("for UCS")
+        print((ucs2(dict_graph, src, dst)))
+
+    n = eval(input("enter 1 if you wish to continue:\n"))
---
+++ test.py
@@ -0,0 +1,160 @@
+import pyvis.network
+from queue import PriorityQueue
+import time
+
+pyvis_graph = pyvis.network.Network(notebook=True, height="100%", width="100%")
+
+dict_graph = {}
+
+# Read the data.txt file
+with open('cities.txt', 'r') as f:
+    for l in f:
+        city_a, city_b, p_cost = l.split()
+        if city_a not in dict_graph:
+            dict_graph[city_a] = {}
+        dict_graph[city_a][city_b] = float(p_cost)
+        if city_b not in dict_graph:
+            dict_graph[city_b] = {}
+        dict_graph[city_b][city_a] = float(p_cost)
+
+
+def BreadthFirstSearch(graph, src, dst):
+    count = 0
+    q = [(src, [src], 0)]
+    visited = {src}
+    while q:
+        # count = count + 1
+        (node, path, cost) = q.pop(0)
+        for temp in list(graph[node].keys()):
+            count = count + 1
+            if temp == dst:
+                return path + [temp], cost + graph[node][temp], str(count),visited
+            else:
+                if temp not in visited:
+                    visited.add(temp)
+                    q.append((temp, path + [temp], cost + graph[node][temp]))
+
+
+def fun(item):
+    return item[2]
+
+
+def ucs(graph, src, dst):
+    frontier = [(src, [src], 0)]
+    visited = {src}
+    while frontier:
+        (node, path, cost) = frontier.pop()
+        for temp in list(graph[node].keys()):
+            if temp == dst:
+                return path + [temp], cost + graph[node][temp]
+            else:
+                if temp not in visited:
+                    visited.add(temp)
+                    # for k in frontier:
+                    # x =(temp, path + [temp], cost + graph[node][temp])
+                    frontier.append((temp, path + [temp], cost + graph[node][temp]))
+                    frontier.sort(key=fun)
+
+
+def ucs2(graph, src, dst):
+    count = 0
+    q = [(src, [src], 0)]
+    visited = {src}
+    while q:
+        # count = count + 1
+        (node, path, cost) = q.pop(0)
+        for temp in list(graph[node].keys()):
+            count = count + 1
+            if temp == dst:
+                return path + [temp], cost + graph[node][temp], str(count)
+            else:
+                if temp not in visited:
+                    visited.add(temp)
+                    q.append((temp, path + [temp], cost + graph[node][temp]))
+                    q.sort(key=fun)
+
+
+def DepthFirstSearch(graph, src, dst):
+    stack = [(src, [src], 0)]
+    visited = {src}
+    while stack:
+        (node, path, cost) = stack.pop()
+        for temp in list(graph[node].keys()):
+            if temp == dst:
+                return path + [temp], cost + graph[node][temp]
+            else:
+                if temp not in visited:
+                    visited.add(temp)
+                    stack.append((temp, path + [temp], cost + graph[node][temp]))
+
+
+def IterativeDeepening(graph, src, dst):
+    sizer = 0
+    level = 0
+    count = 0
+    stack = [(src, [src], 0)]
+    visited = {src}
+    while True:
+        level += 1
+        while stack:
+            if count <= level:
+                count = 0
+                (node, path, cost) = stack.pop()
+                for temp in list(graph[node].keys()):
+                    sizer = sizer + 1
+                    if temp == dst:
+                        return path + [temp], cost + graph[node][temp], str(sizer)
+                    else:
+                        if temp not in visited:
+                            visited.add(temp)
+                            count += 1
+                            stack.append((temp, path + [temp], cost + graph[node][temp]))
+            else:
+                q = stack
+                visited_bfs = {src}
+                while q:
+                    (node, path, cost) = q.pop(0)
+                    for temp in list(graph[node].keys()):
+                        sizer = sizer + 1
+                        if temp == dst:
+                            return path + [temp], cost + graph[node][temp], str(sizer)
+                        else:
+                            if temp not in visited_bfs:
+                                visited_bfs.add(temp)
+                                q.append((temp, path + [temp], cost + graph[node][temp]))
+                break
+
+
+# print((ucs(dict_graph, "Riyadh", "Abha")))
+# print((ucs2(dict_graph, "Riyadh", "Shaqra")))
+# print((BreadthFirstSearch(dict_graph, "Riyadh", "Shaqra")))
+# print((DepthFirstSearch(dict_graph, "Riyadh", "Shaqra")))
+# print((IterativeDeepening(dict_graph, "Riyadh", "Shaqra")))
+xx, yy, zz,newXX = BreadthFirstSearch(dict_graph, "Riyadh", "Abha")
+
+start1 = time.time()
+# print((BreadthFirstSearch(dict_graph, "r", "n")))
+print((BreadthFirstSearch(dict_graph, "Al-Laith", "Thar")))
+
+time.sleep(1)
+end1 = time.time()
+start2 = time.time()
+# print((IterativeDeepening(dict_graph, "r", "n")))
+print((IterativeDeepening(dict_graph, "Al-Laith", "Thar")))
+time.sleep(1)
+end2 = time.time()
+print(end1 - start1)
+print(end2 - start2)
+#
+with open("cities.txt", 'r') as f:
+    for l in f:
+        color = "red"
+        a, b, c = l.split()
+        if a in xx or b in xx:
+            color = "green"
+        pyvis_graph.add_node(a, label=a, title=a, color=color)
+        pyvis_graph.add_node(b, label=b, title=b, color=color)
+        pyvis_graph.add_edge(a, b, label=c, title=c, color=color)
+
+pyvis_graph.force_atlas_2based()
+pyvis_graph.show("graph.html")
